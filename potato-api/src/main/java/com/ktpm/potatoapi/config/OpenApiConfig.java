7e6655af283b2ceff73c934a28fba27949f812a4
tanpuhedu <3122411191@sv.sgu.edu.vn>
2025-09-20 22:10:26 +0700

    integrate OpenApi

---
+++ potato-api/src/main/java/com/ktpm/potatoapi/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.ktpm.potatoapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Potato API document")
+                .version("v1.0.0")
+                .description("Potato API Service");
+
+        return new OpenAPI().info(info);
+    }
+}

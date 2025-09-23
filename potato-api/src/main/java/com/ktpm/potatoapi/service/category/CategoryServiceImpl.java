a1a340554ac7ced6479f84ebd93e1175b19eddc1
tanpuhedu <3122411191@sv.sgu.edu.vn>
2025-09-23 21:00:10 +0700

    #10 - refactor services

--- potato-api/src/main/java/com/ktpm/potatoapi/service/category/CategoryServiceImpl.java
+++ potato-api/src/main/java/com/ktpm/potatoapi/service/category/CategoryServiceImpl.java
@@ -1,19 +1,20 @@
 package com.ktpm.potatoapi.service.category;
 
+import com.ktpm.potatoapi.dto.request.CategoryRequest;
 import com.ktpm.potatoapi.entity.Category;
-import com.ktpm.potatoapi.exception.LogicCustomException;
+import com.ktpm.potatoapi.exception.AppException;
+import com.ktpm.potatoapi.exception.ErrorCode;
 import com.ktpm.potatoapi.mapper.CategoryMapper;
-import com.ktpm.potatoapi.payload.request.CreateCategoryRequest;
-import com.ktpm.potatoapi.payload.response.CategoryResponse;
+import com.ktpm.potatoapi.dto.response.CategoryResponse;
 import com.ktpm.potatoapi.repository.CategoryRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,47 +27,42 @@ public class CategoryServiceImpl implements CategoryService {
     CategoryMapper categoryMapper;
 
     @Override
-    public void createCategory(CreateCategoryRequest createCategoryRequest) {
-        Category category = categoryMapper.toEntity(createCategoryRequest);
-        categoryRepository.save(category);
+    public void createCategory(CategoryRequest categoryRequest) {
+        Category category = categoryMapper.toEntity(categoryRequest);
+
+        try {
+            categoryRepository.save(category);
+        } catch (DataIntegrityViolationException e) {
+            throw new AppException(ErrorCode.CATEGORY_EXISTED);
+        }
+
         log.info("Category created: {}", category.getName());
     }
 
     @Override
-    public void update(CreateCategoryRequest createCategoryRequest, Long id) {
-        Optional<Category> categoryOptional = categoryRepository.findById(id);
-        if(categoryOptional.isEmpty()) {
-            log.error("Category id does not exists: {}", id);
-            LogicCustomException logicCustomException = new LogicCustomException();
-            logicCustomException.setMessage("Category does not exists");
-            logicCustomException.setCode(404);
-            throw logicCustomException;
-        }
-        Category category = categoryOptional.get();
-        category.setName(createCategoryRequest.getName());
+    public void update(CategoryRequest categoryRequest, Long id) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.CATEGORY_NOT_FOUND));
+
+        category.setName(categoryRequest.getName());
         categoryRepository.save(category);
         log.info("Category updated: {} {}", category.getId(), category.getName());
     }
 
     @Override
     public void delete(Long id) {
-        Optional<Category> categoryOptional = categoryRepository.findById(id);
-        if(categoryOptional.isEmpty()) {
-            log.error("Category id does not exists: {}", id);
-            LogicCustomException logicCustomException = new LogicCustomException();
-            logicCustomException.setMessage("Category does not exists");
-            logicCustomException.setCode(404);
-            throw logicCustomException;
-        }
-        Category category = categoryOptional.get();
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.CATEGORY_NOT_FOUND));
+
         categoryRepository.delete(category);
         log.info("Category deleted: {} {}", category.getId(), category.getName());
     }
 
     @Override
     public List<CategoryResponse> listCategory() {
-        List<Category> categoryList = categoryRepository.findAll();
-        log.info("List category successful");
-        return categoryMapper.toDtoList(categoryList);
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::toResponse)
+                .toList();
     }
 }
--- potato-api/src/main/java/com/ktpm/potatoapi/service/user/UserServiceImpl.java
+++ potato-api/src/main/java/com/ktpm/potatoapi/service/user/UserServiceImpl.java
@@ -1,15 +1,13 @@
 package com.ktpm.potatoapi.service.user;
 
-import com.ktpm.potatoapi.exception.LogicCustomException;
-import com.ktpm.potatoapi.payload.request.UserLogInRequest;
-import com.ktpm.potatoapi.payload.request.UserRequest;
-import com.ktpm.potatoapi.payload.request.UserSignUpRequest;
-import com.ktpm.potatoapi.payload.response.UserLogInResponse;
-import com.ktpm.potatoapi.payload.response.UserResponse;
 import com.ktpm.potatoapi.entity.User;
-import com.ktpm.potatoapi.enums.EntityStatus;
 import com.ktpm.potatoapi.enums.Role;
+import com.ktpm.potatoapi.exception.AppException;
+import com.ktpm.potatoapi.exception.ErrorCode;
 import com.ktpm.potatoapi.mapper.UserMapper;
+import com.ktpm.potatoapi.dto.request.UserLogInRequest;
+import com.ktpm.potatoapi.dto.request.UserSignUpRequest;
+import com.ktpm.potatoapi.dto.response.UserLogInResponse;
 import com.ktpm.potatoapi.repository.UserRepository;
 import com.ktpm.potatoapi.utils.JwtUtils;
 import jakarta.servlet.http.HttpServletRequest;
@@ -39,53 +37,34 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserLogInResponse signUp(UserSignUpRequest userSignUpRequest , HttpServletRequest httpRequest) {
-        Optional<User> checker = userRepository.findByEmail(userSignUpRequest.getEmail());
-        if (checker.isPresent()) {
-            log.error("Email already exist");
-            LogicCustomException logicCustomException = new LogicCustomException();
-            logicCustomException.setMessage("Email is already in use");
-            logicCustomException.setCode(400);
-            throw logicCustomException;
-        }
+        if (userRepository.existsByEmail(userSignUpRequest.getEmail()))
+            throw new AppException(ErrorCode.USER_EXISTED);
 
         // Create new user
-        User user = userMapper.mapSignUpToEntity(userSignUpRequest);
+        User user = userMapper.mapSignUpRequestToEntity(userSignUpRequest);
         user.setPassword(passwordEncoder.encode(userSignUpRequest.getPassword()));
-        User savedUser = userRepository.save(user);
+        userRepository.save(user);
 
-        //Generate Jwt token
-        UserLogInResponse userLogInResponse = new UserLogInResponse();
-        userLogInResponse.setFullName(savedUser.getFullName());
-        userLogInResponse.setEmail(savedUser.getEmail());
-        userLogInResponse.setToken(JwtUtils.createToken(savedUser, httpRequest));
-        log.info("{} sign up success", savedUser.getEmail());
+        // Generate Jwt token
+        UserLogInResponse userLogInResponse = userMapper.toLogInResponse(user);
+        userLogInResponse.setToken(JwtUtils.createToken(user, httpRequest));
+        log.info("{} sign up success", user.getEmail());
 
         return userLogInResponse;
     }
 
     @Override
-    public UserLogInResponse logIn(UserLogInRequest userLogInRequest, HttpServletRequest request) {
-        Optional<User> checker = userRepository.findByEmail(userLogInRequest.getEmail());
-        if(checker.isEmpty()) {
-            log.error("Email or password is incorrect");
-            LogicCustomException logicCustomException = new LogicCustomException();
-            logicCustomException.setMessage("Username or password is incorrect");
-            logicCustomException.setCode(401);
-            throw logicCustomException;
-        }
-        if(!passwordEncoder.matches(userLogInRequest.getPassword(), checker.get().getPassword())) {
-            log.error("Email or password is incorrect");
-            LogicCustomException logicCustomException = new LogicCustomException();
-            logicCustomException.setMessage("Username or password is incorrect");
-            logicCustomException.setCode(401);
-            throw logicCustomException;
-        }
+    public UserLogInResponse logIn(UserLogInRequest userLogInRequest, HttpServletRequest httpRequest) {
+        User user = userRepository.findByEmail(userLogInRequest.getEmail())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+
+        boolean isAuthenticated = passwordEncoder.matches(userLogInRequest.getPassword(),user.getPassword());
+        if(!isAuthenticated)
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
 
         // Generate token
-        UserLogInResponse userLogInResponse = new UserLogInResponse();
-        userLogInResponse.setFullName(checker.get().getFullName());
-        userLogInResponse.setEmail(checker.get().getEmail());
-        userLogInResponse.setToken(JwtUtils.createToken(checker.get(), request));
+        UserLogInResponse userLogInResponse = userMapper.toLogInResponse(user);
+        userLogInResponse.setToken(JwtUtils.createToken(user, httpRequest));
         log.info("{} log in success", userLogInResponse.getFullName());
 
         return userLogInResponse;
@@ -94,67 +73,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> checker = userRepository.findByEmail(username);
-        if (checker.isEmpty()) {
+        if (checker.isEmpty())
             throw new UsernameNotFoundException(username);
-        }
+
         User user = checker.get();
-        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
         Role role = user.getRole();
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
         SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(role.name());
         grantedAuthorities.add(simpleGrantedAuthority);
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), grantedAuthorities);
+
+        return new org.springframework.security.core.userdetails.User
+                (user.getEmail(), user.getPassword(), grantedAuthorities);
     }
-
-    // Phú thành code
-    /*
-    @Override
-    public List<UserResponse> getAll() {
-        return userRepository.findAll().stream()
-                .map(mapper::toResponse)
-                .toList();
-    }
-
-    @Override
-    public UserResponse getById(Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("not found"));
-
-        return mapper.toResponse(user);
-    }
-
-    @Override
-    public UserResponse createCustomer(UserRequest request) {
-        if (userRepository.existsByUsername(request.getUsername()))
-            throw new RuntimeException("existed");
-
-        User user = mapper.toEntity(request);
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-        user.setRole(Role.CUSTOMER.name());
-        return mapper.toResponse(userRepository.save(user));    }
-
-    @Override
-    public UserResponse update(Long id, UserRequest request) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("not found"));
-
-        mapper.update(user, request);
-
-        return mapper.toResponse(userRepository.save(user));
-    }
-
-    @Override
-    public UserResponse updateStatus(Long id, int status) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("not found"));
-
-        // validate status from request param
-        if (status != EntityStatus.INACTIVE.getCode() && status != EntityStatus.ACTIVE.getCode())
-            throw new RuntimeException("invalid status");
-
-        user.setStatus(status);
-
-        return mapper.toResponse(userRepository.save(user));
-    }
-
-     */
 }
